cb26c7512dbc7e4939545ad43d1d92e6ba57b958
Jaishika prasad <jaishikaprasad@gmail.com>
2023-09-01 08:26:13 +0530

    Added Problem : Count Primes

--- problems/count_primes.py
+++ problems/count_primes.py
@@ -1,3 +1,21 @@
+# Leetcode Ques : 204 [Count Primes]
+
+# Given an integer n, return the number of prime numbers that are strictly less than n.
+
+# Sieve of Eratosthenes logic
+def count_primes(n):
+    primes = 0
+    board = [0]*n
+    for i in range(1,n):
+        if board[i] == 0:  # 0 denote prime numbers
+            primes += 1
+            for j in range(i*i, n, i):
+                board[j] = 1  # 1 denote composite numbers
+
+    return primes
+
+# Brute Force Method - TLE
+
 def count_primes(n):
     primes = 0
     for num in range(1,n):
