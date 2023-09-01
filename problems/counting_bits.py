b141565cc9b8750e2a296f1aafb617c27594fe7c
Jaishika prasad <jaishikaprasad@gmail.com>
2023-09-01 14:52:19 +0530

    Added Problem : Counting Bits

--- problems/counting_bits.py
+++ problems/counting_bits.py
@@ -1,3 +1,9 @@
+# Leetcode Ques : 338 [Counting Bits]
+
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), 
+# ans[i] is the number of 1's in the binary representation of i.
+
+# Dynamic Programming
 def counting_bits(n):
     dp = [0]
     for i in range(1, n+1):
@@ -8,13 +14,15 @@ def counting_bits(n):
     return dp
 
 # Brute Force Approach - O(nlog)
-# def counting_bits(n):
-#     result = []
-#     for i in range(n+1):
-#         binary = bin(i)
-#         ones = binary.count("1")
-#         result.append(ones)
+def counting_bits(n):
+    result = []
+    for i in range(n+1):
+        binary = bin(i)
+        ones = binary.count("1")
+        result.append(ones)
 
-#     return result
+    return result
 
 print(counting_bits(5))
+
+# Output : [0, 1, 1, 2, 1, 2]
